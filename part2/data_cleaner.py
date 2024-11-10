ad6b5214c58057f8f37793c6865fda8170e14f18
michaelmengistu2 <michaelmengistu.mm@gmail.com>
2024-11-10 15:57:19 -0600

    fix wording

--- part2/data_cleaner.py
+++ part2/data_cleaner.py
@@ -20,7 +20,7 @@ filter_df = spark.sql("SELECT * FROM table WHERE price > 0 and qty > 0")
 
 filter_df.show()
 
-#Find the most popular products
+#Findthe qty sold for each product name
 pop_products_df = filter_df.select('product_name', 'price', 'qty', 'payment_transaction_success')
 pop_products_df = pop_products_df.filter(pop_products_df.payment_transaction_success == "Y")
 pop_products_df = pop_products_df.select('product_name', 'price', 'qty')
@@ -28,7 +28,7 @@ pop_products_df = pop_products_df.groupBy("product_name").sum().sort("sum(qty)",
 pop_products_df.show()
 
 
-#Find the qty sold for each product name
+#Find automobile product sales by gender
 car_df = filter_df.select('product_category','product_name','price', 'qty', 'gender', 'payment_transaction_success')
 car_df = car_df.filter(car_df.payment_transaction_success == "Y")
 car_df = car_df.select('product_category','product_name','price', 'qty', 'gender')
